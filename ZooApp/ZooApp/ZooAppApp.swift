d814e3253f66cea2b958cdde676aaab1563a9a93
Süleyman Üren <suleymanuren07@gmail.com>
2023-07-03 15:37:37 +0300

    ADD assets - data json

---
+++ ZooApp/ZooApp/ZooAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZooAppApp.swift
+//  ZooApp
+//
+//  Created by Bulut Sistem on 3.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct ZooAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
