b571fd8c6e585be9947bbf8861ac005930adf0d4
Süleyman Üren <suleymanuren07@gmail.com>
2023-07-04 09:27:31 +0300

    ADD animal detail page

--- ZooApp/ZooApp/ui/AnimalDetail.swift
+++ ZooApp/ZooApp/ui/AnimalDetail.swift
@@ -10,7 +10,10 @@ import SwiftUI
 struct AnimalDetail: View {
     let animalsData : AnimalsModel
     var body: some View {
+        ScrollView {
             VStack{
+                //animal title start
+                Group{
                     Image(animalsData.image ?? "")
                         .resizable()
                         .scaledToFit()
@@ -18,20 +21,34 @@ struct AnimalDetail: View {
                             RoundedRectangle(cornerRadius: 12)
                         )
                     
-                
-               
-                Text(animalsData.name ?? "")
-                    .fontWeight(.heavy)
-                    .font(.title2)
-                Divider()
-                 .frame(width: 100,height: 5)
-                 .background(Color.accentColor)
-                
-                Text(animalsData.headline ?? "")
-                    .fontWeight(.medium)
-                    .font(.headline)
-                    .foregroundColor(.accentColor)
-                
+                    Text(animalsData.name?.uppercased() ?? "")
+                        .font(.largeTitle)
+                        .fontWeight(.heavy)
+                        .multilineTextAlignment(.center)
+                        .padding(.vertical, 8)
+                        .foregroundColor(.primary)
+                        .background(
+                            Color.accentColor
+                                .frame(height: 6)
+                                .offset(y: 24)
+                                .opacity(0.75)
+                        )
+                    
+                    
+                    //                Divider()
+                    //                 .frame(width: 100,height: 5)
+                    //                 .background(Color.accentColor)
+                    
+                    Text(animalsData.headline ?? "")
+                        .fontWeight(.medium)
+                        .font(.headline)
+                        .foregroundColor(.accentColor)
+                        .multilineTextAlignment(.center)
+                }
+                //animal title end
+
+
+                //animal gallery start
                 Spacer()
                 HStack{
                     Image(systemName: "photo")
@@ -40,7 +57,6 @@ struct AnimalDetail: View {
                         .fontWeight(.light)
                         .font(.title2)
                 }
-                
                 TabView{
                     ForEach(animalsData.gallery ?? [],id: \.self) {animal in
                         Image(animal ?? "")
@@ -50,15 +66,109 @@ struct AnimalDetail: View {
                                 RoundedRectangle(cornerRadius: 12)
                             )
                             .padding(.horizontal,2)
-
+                        
                     }
                     
                 }.tabViewStyle(PageTabViewStyle())
                     .frame(height: 250)
                     .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-
-        
+                //animal gallery end
+                
+                //facts start
+                Group{
+                    HStack{
+                        Image(systemName: "info.circle")
+                            .foregroundColor(.accentColor)
+                        Text("Did you know ?")
+                            .fontWeight(.heavy)
+                            .font(.title3)
+                    }
+                    TabView{
+                        ForEach(animalsData.fact ?? [],id: \.self) {animal in
+                            Text(animal)
+                                .fontWeight(.light)
+                                .foregroundColor(.white)
+                                .multilineTextAlignment(.center)
+                                .lineLimit(3)
+                                .padding()
+                        }
+                        
+                    }.tabViewStyle(PageTabViewStyle())
+                        .frame(height: 200)
+                        .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                        .background(.gray.opacity(0.2))
+                        .clipShape(
+                            RoundedRectangle(cornerRadius: 12)
+                        )
+                }
+                //facts end
+                //maps start
+                Group{
+                    HStack{
+                        Image(systemName: "map")
+                            .foregroundColor(.accentColor)
+                        Text("National Parks")
+                            .fontWeight(.light)
+                            .font(.title2)
+                    }
+                    AnimalDetailMap()
+                }
+                //maps end
+                
+                //animal about start
+                Group{
+                    HStack{
+                        Image(systemName: "info.circle")
+                            .foregroundColor(.accentColor)
+                        Text("All about \(animalsData.name ?? "")")
+                            .fontWeight(.light)
+                            .font(.title2)
+                    }
+                    Text(animalsData.description ?? "")
+                        .fontWeight(.light)
+                        .font(.body)
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                }
+                //animal about end
+                //wikipedia start
+                Group{
+                    Spacer()
+                    HStack{
+                        Image(systemName: "info.circle")
+                            .foregroundColor(.accentColor)
+                        Text("Learn more")
+                            .fontWeight(.light)
+                            .font(.title2)
+                    }
+                    HStack{
+                        Image(systemName: "network")
+                        Text("Wikipedia")
+                        Spacer()
+                        Image(systemName: "arrow.up.right.square")
+                            .foregroundColor(Color.accentColor)
+                        Text(animalsData.name ?? "")
+                            .foregroundColor(Color.accentColor)
+                        
+                    }
+                    .padding()
+                    .onTapGesture {
+                        
+                        if let url = URL(string: animalsData.link ?? "") {
+                            UIApplication.shared.open(url)
+                        }
+                    }
+                    .background(.bar)
+                    .clipShape(
+                        RoundedRectangle(cornerRadius: 12)
+                    )
+                }
+                
+                //wikipedia end
+                
             }.navigationTitle("Learn about \( animalsData.name ?? "")")
+                .padding()
+        }
     }
 }
 
